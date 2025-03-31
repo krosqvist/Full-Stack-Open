7d7924fa72e2fb36c6a45ae8a23578f072b4665e
Kim Rosqvist <krosqvist1@gmail.com>
2025-03-31 11:56:08 +0300

    Add morgan to puhelinluettelo

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('code', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :code'))
 
 let persons = [
     {
