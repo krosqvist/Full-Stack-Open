eef313e967acb4679a9fbe6a062a9af2e9ba269e
Kim Rosqvist <krosqvist1@gmail.com>
2026-08-13 00:21:37 +0300

    Task 6.15 done

---
+++ testit/osa6_testit/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+import NoteForm from './components/NoteForm'
+import NoteList from './components/NoteList'
+import VisibilityFilter from './components/VisibilityFilter'
+import { useNoteActions } from './services/store'
+
+const App = () => {
+  const { initialize } = useNoteActions()
+
+  useEffect(() => {
+    initialize()
+  }, [initialize])
+
+  return (
+    <div>
+      <NoteForm />
+      <VisibilityFilter />
+      <NoteList />
+    </div>
+  )
+}
+export default App
---
+++ testit/osa6_testit/src/services/store.js
@@ -0,0 +1,41 @@
+import { create } from 'zustand'
+import noteService from './notes'
+
+const useNoteStore = create((set, get) => ({
+  notes: [],
+  filter: 'all',
+  actions: {
+    add: async (content) => {
+      const newNote = await noteService.createNew(content)
+      set(state => ({ notes: state.notes.concat(newNote) }))
+    },
+
+    toggleImportance: async (id) => {
+      const note = get().notes.find(n => n.id === id)
+      const updated = await noteService.update(
+        id, { ...note, important: !note.important }
+      )
+      set(state => ({
+        notes: state.notes.map(n => n.id === id ? updated : n)
+      }))
+    },
+
+    setFilter: value => set(() => ({ filter: value })),
+    initialize: async () => {
+      const notes = await noteService.getAll()
+      set(() => ({ notes }))
+    },
+  }
+}))
+
+export default useNoteStore
+
+export const useNotes = () => {
+  const notes = useNoteStore((state) => state.notes)
+  const filter = useNoteStore((state) => state.filter)
+  if (filter === 'important') return notes.filter(n => n.important)
+  if (filter === 'nonimportant') return notes.filter(n => !n.important)
+  return notes
+}
+export const useFilter = () => useNoteActions(state => state.filter)
+export const useNoteActions = () => useNoteStore(state => state.actions)
